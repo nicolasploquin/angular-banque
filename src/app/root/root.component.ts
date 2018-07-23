ca91d0b3ae2d835de823694ca9f04a90a66c5bbb
Nicolas PLOQUIN <nicolas.ploquin@gmail.com>
2018-07-23 12:29:39 +0200

    ajout composant stateless/shared message

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./root.component.css']
 })
 export class RootComponent {
-  username: string = 'Ben';
+  username: string = '';
   showAuthForm = false;
 
   ajd: Date = new Date();
