1861c86bdc4010b5f896b90b8e87ca14a8cfdc0e
Nicolas PLOQUIN <nicolas.ploquin@gmail.com>
2018-12-19 23:16:11 +0100

    explore databinding with reactive form

--- src/app/client-reactive-form/client-reactive-form.component.ts
+++ src/app/client-reactive-form/client-reactive-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import {defaultIfEmpty, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-client-reactive-form',
@@ -7,6 +8,8 @@ import {FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./client-reactive-form.component.css']
 })
 export class ClientReactiveFormComponent implements OnInit {
+
+  log = {};
 
   clientForm = new FormGroup({
     nom : new FormControl(''),
@@ -16,13 +19,18 @@ export class ClientReactiveFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // initialisation d'un champ
+    // this.clientForm.get('nom').setValue('Martin');
+    this.clientForm.patchValue({
+      nom: 'Dupont'
+    });
   }
 
   get client() {
-    return this.clientForm.valueChanges;
+    return this.clientForm.valueChanges; //.pipe(map(val => val?val:this.clientForm.value));
   }
 
   enregistrer() {
-
+    this.log = this.clientForm.value;
   }
 }
